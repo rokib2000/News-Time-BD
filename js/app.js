9dd8d653e106f1226c9fadf94c428c88cb59471f
rokib2000 <rokib0w1@gmail.com>
2022-09-03 04:42:14 +0600

    added design and category view form Api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log("News Time");
+// load All catagories
+
+const loadCategories = async () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayCategories(data.data.news_category);
+};
+
+// View all categories
+
+const displayCategories = (categories) => {
+  const categoryItem = document.getElementById("category-item");
+
+  categories.map((category) => {
+    categoryItem.innerHTML += `
+    <li   class="nav-item" >
+        <a  onclick="loadCategoryNews('${category.category_id}')"  class="nav-link color-Gray52" href="#">
+            ${category.category_name}
+        </a>
+    </li>`;
+  });
+};
+
+// Load category news
+
+const loadCategoryNews = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.data);
+};
+
+loadCategories();
